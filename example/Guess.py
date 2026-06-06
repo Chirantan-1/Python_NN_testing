0589f8457234c0486ff40b3aed2b010e6ffc80d6
Chirantan-1 <chirurdp@gmail.com>
2026-06-06 15:55:05 +0530

    Update Guess.py

--- example/Guess.py
+++ example/Guess.py
@@ -1,18 +1,12 @@
 from TheNN import NeuralNetwork
 from NNDrawer import NNDrawer
 
-nn = NeuralNetwork(save_file="model.pkl")
+nn = NeuralNetwork(
+    save_file="model.pkl"
+)
 
 nn.load()
 
-drawer = NNDrawer(
-    nn=nn,
-    input_size=784,
-    screen_size=600,
-    output_size=10,
-    output_labels=["0","1","2","3","4","5","6","7","8","9"],
-    rows=28,
-    cols=28
-)
+drawer = NNDrawer(nn=nn, input_size=784, screen_size=600, output_size=10, output_labels=["0","1","2","3","4","5","6","7","8","9"], rows=28, cols=28)
 
 drawer.run("Digit Recognition")
